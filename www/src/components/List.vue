c76be4a2167893a4906a95fcc91cc7de1bcaa963
Sean MacKenzie <sean.andrew.mackenzie@gmail.com>
2017-11-30 16:33:39 -0700

    fixed merge

--- www/src/components/List.vue
+++ www/src/components/List.vue
@@ -1,13 +1,8 @@
 <template>
     <div>
-<<<<<<< HEAD
         <h1>{{name}}</h1>
         <h6>{{description}}</h6>
         <span @click="removeList(listId, board._id)" class="glyphicon glyphicon-trash"></span>
-=======
-        <h2>{{name}}</h2>
-        <h5>{{description}}</h5>
->>>>>>> a450d594830eb23aba0ae48279ed8ec146aecb2b
     </div>
 
 </template>
