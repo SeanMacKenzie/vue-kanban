0ec5436eeb88017856380233cdd20fc4dc921cf1
Cameron Sharp <cameron.sharp1980@gmail.com>
2017-12-03 00:25:52 -0700

    Added some basic styling and positions (bootstrap) to the login and boards pages. Boards page drawing the add boards form and boards with dynamic offsets

--- www/src/components/Boards.vue
+++ www/src/components/Boards.vue
@@ -1,30 +1,89 @@
 <template>
-  <div>
-    <div>
-      <button @click="logout">Logout</button>
+  <div class="boards-page container-fluid">
+    <div class="row">
+      <div class="col-sm-4 col-sm-offset-4">
+        <h3>{{user.name}}'s Boards</h3>
+      </div>
+      <div class="top-bar col-sm-1 col-sm-offset-3">
+        <div class="pull-right">
+          <button @click="logout">Logout</button>
+        </div>
+      </div>
     </div>
-    <div>
+    <div class="row">
 
-      <form class="form" @submit.prevent="createBoard">
-        <div class="form-group">
-          <label for="name">Board name</label>
-          <input class="form-control" type="text" name="name" placeholder="Board name" v-model='newBoard.name' required>
-        </div>
-        <div class="form-group">
-          <label for="description">Description</label>
-          <input class="form-control" type="text" name="description" v-model='newBoard.description' required>
-        </div>
-        <div class="form-group">
-          <button type="submit">Create Board</button>
-        </div>
-      </form>
       <!-- <button @click="createBoard">Add Board</button> -->
-      <ul>
-        <li v-for="board in boards">
+
+      <div class="col-sm-2 col-sm-offset-5 thumbnail" v-if="boards.length == 0">
+        <form class="form" @submit.prevent="createBoard">
+          <div class="form-group">
+            <label for="name">Board name</label>
+            <input class="form-control" type="text" name="name" placeholder="Board name" v-model='newBoard.name' required>
+          </div>
+          <div class="form-group">
+            <label for="description">Description</label>
+            <input class="form-control" type="text" name="description" v-model='newBoard.description' required>
+          </div>
+          <div class="form-group">
+            <button type="submit">Create Board</button>
+          </div>
+        </form>
+      </div>
+
+      <div v-for="(board, i) in boards">
+
+        <div class="col-sm-2 thumbnail col-sm-offset-1" v-if="i % 3 == 0">
           <router-link :to="'/boards/'+board._id">{{board.name}}</router-link>
           <span @click="removeBoard(board)" class="glyphicon glyphicon-trash"></span>
-        </li>
-      </ul>
+          <p>{{board.description}}</p>
+        </div>
+
+        <div class="col-sm-2 col-sm-offset-2 thumbnail" v-else>
+          <router-link :to="'/boards/'+board._id">{{board.name}}</router-link>
+          <span @click="removeBoard(board)" class="glyphicon glyphicon-trash"></span>
+          <p>{{board.description}}</p>
+        </div>
+
+        <div v-if="i == boards.length - 1">
+
+
+          <div class="col-sm-2 col-sm-offset-1 thumbnail" v-if="boards.length % 3 == 0">
+            <form class="form" @submit.prevent="createBoard">
+              <div class="form-group">
+                <label for="name">Board name</label>
+                <input class="form-control" type="text" name="name" placeholder="Board name" v-model='newBoard.name' required>
+              </div>
+              <div class="form-group">
+                <label for="description">Description</label>
+                <input class="form-control" type="text" name="description" v-model='newBoard.description' required>
+              </div>
+              <div class="form-group">
+                <button type="submit">Create Board</button>
+              </div>
+            </form>
+          </div>
+
+          <div class="col-sm-2 col-sm-offset-2 thumbnail" v-else>
+            <form class="form" @submit.prevent="createBoard">
+              <div class="form-group">
+                <label for="name">Board name</label>
+                <input class="form-control" type="text" name="name" placeholder="Board name" v-model='newBoard.name' required>
+              </div>
+              <div class="form-group">
+                <label for="description">Description</label>
+                <input class="form-control" type="text" name="description" v-model='newBoard.description' required>
+              </div>
+              <div class="form-group">
+                <button type="submit">Create Board</button>
+              </div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+
+
     </div>
   </div>
 </template>
@@ -69,13 +128,9 @@
 </script>
 
 <style>
-  .jumbotron {
+  /* .jumbotron {
     height: 300px;
     border-top: 0;
     margin-top: 0;
-  }
-
-  .container {
-    width: 100%;
-  }
+  } */
 </style>
--- www/src/components/Login.vue
+++ www/src/components/Login.vue
@@ -1,50 +1,60 @@
 <template>
-    <div class="login">
-        <h1>Kanban!</h1>
-        <p>please login to continue:</p>
-        <!-- Add error message if login fails -->
+    <div class="login container-fluid">
         <h5 class="text-danger" v-if="error">
             <b>{{error}}</b>
         </h5>
-        <div v-if="loginForm" class="login">
-            <form class="form" @submit.prevent="submitLogin">
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input class="form-control" type="email" name="email" placeholder="your@email.com" v-model='login.email' required>
+        <div class="row">
+            <div class="login-box thumbnail col-sm-4 col-sm-offset-4">
+                <h1>(Project name) Kanban!</h1>
+                <p>please login to continue:</p>
+                <!-- Add error message if login fails -->
+                <div class="form-box row">
+                    <div class="form-col col-sm-8 col-sm-offset-2">
+
+
+
+                        <div v-if="loginForm" class="login">
+                            <form class="form" @submit.prevent="submitLogin">
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input class="form-control" type="email" name="email" placeholder="your@email.com" v-model='login.email' required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input class="form-control" type="password" name="password" v-model='login.password' required>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit">Login</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div v-else class="register">
+                            <form class="form" @submit.prevent="submitRegister">
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input class="form-control" type="text" name="name" placeholder="Name" v-model='register.name' required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input class="form-control" type="email" name="email" placeholder="your@email.com" v-model='register.email' required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input class="form-control" type="password" name="password" v-model='register.password' required>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit">Register</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input class="form-control" type="password" name="password" v-model='login.password' required>
-                </div>
-                <div class="form-group">
-                    <button type="submit">Login</button>
-                </div>
-            </form>
+                <p v-if="loginForm" @click="toggleLoginForm" class="action">Not a user? Click here to register</p>
+                <p v-else @click="toggleLoginForm" class="action">Already a user? Click here to login</p>
+
+            </div>
         </div>
-        <div v-else class="register">
-            <form class="form" @submit.prevent="submitRegister">
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input class="form-control" type="text" name="name" placeholder="Name" v-model='register.name' required>
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input class="form-control" type="email" name="email" placeholder="your@email.com" v-model='register.email' required>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input class="form-control" type="password" name="password" v-model='register.password' required>
-                </div>
-                <div class="form-group">
-                    <button type="submit">Register</button>
-                </div>
-            </form>
-
-
-        </div>
-
-        <p v-if="loginForm" @click="toggleLoginForm" class="action">Not a user? Click here to register</p>
-        <p v-else @click="toggleLoginForm" class="action">Already a user? Click here to login</p>
     </div>
 </template>
 
